d2ef04ef5826e3f8bc65e2b1f5774151a37927ec
Elena Laakso <elena.laakso@metropolia.fi>
2020-09-10 15:39:14 +0300

    a. hard coded login added

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
 /* eslint-disable max-len */
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
-import List from './components/List';
-
+import Navigator from './navigators/Navigator';
+import { AuthProvider } from './contexts/AuthContext';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <List />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <AuthProvider>
+      <Navigator />
+    </AuthProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
