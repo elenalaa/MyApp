b510961697fb0826bcf376aa6dd10f0c524feac0
Elena Laakso <elena.laakso@metropolia.fi>
2020-09-16 11:42:35 +0300

    upload updated

--- components/FormTextInput.js
+++ components/FormTextInput.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import {StyleSheet, TextInput} from 'react-native';
 import PropTypes from 'prop-types';
+import { Item, Input } from 'native-base';
 
-const FormTextInput = ({style, ...otherProps}) => {
+const FormTextInput = ({ style, error, ...otherProps }) => {
   return (
-    <TextInput
-      style={[styles.textInput, style]}
-      {...otherProps}
-    />
+    <View>
+      <Item>
+        <Input
+          {...otherProps}
+        />
+      </Item>
+      {error !== '' && <Label>{error}</Label>}
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  textInput: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-  },
-});
-
 FormTextInput.propTypes = {
   style: PropTypes.object,
+  error: PropTypes.string,
 };
 
 export default FormTextInput;
--- views/Uploads.js
+++ views/Uploads.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import {Container, Content, Form} from 'native-base';
+import {Button, Container, Content, Form, Text} from 'native-base';
 import FormTextInput from '../components/FormTextInput';
+import {Image, Platform} from 'react-native';
+import useUploadForm from '../hooks/UploadHooks';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 
 const Upload = ({navigation}) => {
+  const [image, setImage] = useState(null);
+
+  const doUpload = () => {
+    const formData = new FormData();
+    // lisätään tekstikentät formDataan
+    formData.append('title', inputs.title);
+    formData.append('description', inputs.description);
+
+    // lisätään tiedosto formDataan
+    const filename = image.split('/').pop();
+    const match = /\.(\w+)$/.exec(filename);
+    let type = match ? `image/${match[1]}` : `image`;
+    if (type === 'image/jpg') type = 'image/jpeg';
+    formData.append('file', {uri: image, name: filename, type});
+
+    console.log('Upload', formData);
+  };
+
+  const pickImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
+  const getPermissionAsync = async () => {
+    if (Platform.OS !== 'web') {
+      const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      console.log('status', status);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+
+  useEffect(() => {
+    getPermissionAsync();
+  }, []);
+
+
+  const {
+    handleInputChange,
+    uploadErrors,
+    inputs,
+  } = useUploadForm();
+
+
   return (
     <Container>
       <Content padder>
+        {image &&
+          <Image
+            source={{uri: image}}
+            style={{height: 400, width: null, flex: 1}}
+          />
+        }
         <Form>
-          <FormTextInput />
+          <FormTextInput
+            autoCapitalize="none"
+            placeholder="title"
+            onChangeText={(txt) => handleInputChange('title', txt)}
+            error={uploadErrors.title}
+          />
+          <FormTextInput
+            autoCapitalize="none"
+            placeholder="description"
+            onChangeText={(txt) => handleInputChange('description', txt)}
+            error={uploadErrors.description}
+          />
         </Form>
+        <Button block onPress={pickImage}>
+          <Text>Choose file</Text>
+        </Button>
+        <Button block onPress={doUpload}>
+          <Text>Upload</Text>
+        </Button>
       </Content>
     </Container>
   );
