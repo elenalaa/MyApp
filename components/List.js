8e6e460461ee297591bff4f65df5d04b60affc57
Elena Laakso <elena.laakso@metropolia.fi>
2020-09-27 23:33:25 +0300

    list.js updated

--- components/List.js
+++ components/List.js
@@ -5,23 +5,27 @@ import {
 import ListItem from './ListItem';
 import PropTypes from 'prop-types';
 import {useLoadMedia} from '../hooks/APIhooks';
+import {AuthContext} from '../contexts/AuthContext';
 
-const List = ({navigation}) => {
-  const mediaArray = useLoadMedia();
+const List = ({navigation, all}) => {
+  const mediaArray = useLoadMedia();	  const {user} = useContext(AuthContext);
+  // console.log(user);
+  const mediaArray = useLoadMedia(all, user.user_id);
 
   return (
     <FlatList
       data={mediaArray}
       keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) =>
-        <ListItem singleMedia={item} navigation={navigation} />
-      }
-    />
+      <ListItem singleMedia={item} navigation={navigation} editable={!all} />
+  }
+  />
   );
 };
 
 List.propTypes = {
   navigation: PropTypes.object,
+  all: PropTypes.bool,
 };
 
 export default List;
